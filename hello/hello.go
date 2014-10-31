922f1897abe04efbf534b132332f609021b49e5f
Michael Nikitochkin <miry.sof@gmail.com>
2014-10-31 14:09:11 +0100

    Step 1: Created simple Go application to revers string.

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/miry/string"
+)
+
+func main() {
+	fmt.Println(string.Reverse("Hello, gopher!"))
+}
---
+++ string/string_test.go
@@ -0,0 +1,22 @@
+package string
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	var tests = []struct {
+		s, want string
+	}{
+		{"Backward", "drawkcaB"},
+		{"", ""},
+		{"Hello Украина", "аниаркУ olleH"},
+	}
+
+	for _, c := range tests {
+		got := Reverse(c.s)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.s, got, c.want)
+		}
+	}
+}
